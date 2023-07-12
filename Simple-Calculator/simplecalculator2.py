2c228e8678360bc7c7d6951bdab2c355b2852a5e
perfumegoddess <queenjossey@gmail.com>
2023-07-12 09:09:48 +0000

    fhhjkld

---
+++ Simple-Calculator/simplecalculator2.py
@@ -0,0 +1,26 @@
+def add(x, y):
+  """Adds two numbers."""
+  return x + y
+
+def subtract(x, y):
+  """Subtracts two numbers."""
+  return x - y
+
+def multiply(x, y):
+  """Multiplies two numbers."""
+  return x * y
+
+def divide(x, y):
+  """Divides two numbers."""
+  return x / y
+
+def main():
+  """Prints the results of some simple calculations."""
+  print(add(1, 2))
+  print(subtract(10, 5))
+  print(multiply(2, 3))
+  print(divide(10, 2))
+
+if __name__ == "__main__":
+  main()
+
