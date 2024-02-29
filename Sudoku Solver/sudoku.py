8bf277518f89d85defe0f9b35444367a475c369a
perfumegoddess <queenjossey@gmail.com>
2024-02-29 13:47:23 +0100

    sudoku commit

---
+++ Sudoku Solver/sudoku.py
@@ -0,0 +1,52 @@
+def find_empty_location(grid):
+    for row in range(9):
+        for col in range(9):
+            if grid[row][col] == 0:
+                return row, col
+    return None, None
+
+def is_valid_location(grid, row, col, num):
+    for x in range(9):
+        if grid[row][x] == num or grid[x][col] == num:
+            return False
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if grid[i + start_row][j + start_col] == num:
+                return False
+    return True
+
+def solve_sudoku(grid):
+    row, col = find_empty_location(grid)
+    if row is None:
+        return True
+    for num in range(1, 10):
+        if is_valid_location(grid, row, col, num):
+            grid[row][col] = num
+            if solve_sudoku(grid):
+                return True
+            grid[row][col] = 0
+    return False
+
+def print_grid(grid):
+    for row in grid:
+        print(" ".join(map(str, row)))
+
+if __name__ == "__main__":
+    grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    if solve_sudoku(grid):
+        print("Sudoku puzzle solved successfully:")
+        print_grid(grid)
+    else:
+        print("No solution exists for the given Sudoku puzzle.")
