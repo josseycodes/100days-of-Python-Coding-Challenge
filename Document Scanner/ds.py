18a8f59a09120adc0f3885f42ed7cbb5c0bc7bb8
perfumegoddess <queenjossey@gmail.com>
2024-04-08 13:22:12 +0100

    document scanner commit

---
+++ Document Scanner/ds.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    return rect
+
+def perspective_transform(image, pts):
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    return warped
+
+def document_scanner(image_path):
+    image = cv2.imread(image_path)
+    ratio = image.shape[0] / 500.0
+    orig = image.copy()
+    image = cv2.resize(image, (int(image.shape[1] / ratio), 500))
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(blurred, 75, 200)
+    contours, _ = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+    for contour in contours:
+        epsilon = 0.02 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+    cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+    warped = perspective_transform(orig, screenCnt.reshape(4, 2) * ratio)
+    return warped
+
+# Test the document scanner
+if __name__ == "__main__":
+    image_path = 'test_document.jpg'
+    scanned_image = document_scanner(image_path)
+    cv2.imshow("Scanned Image", scanned_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
